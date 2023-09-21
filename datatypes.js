ccd58fd4ee381312143bc9775771e6f0234f6ab8
imran <imranejazhashmi@gmail.com>
2023-09-21 16:38:32 +0530

    hii

---
+++ datatypes.js
@@ -0,0 +1,77 @@
+// 5.
+
+var x = "a,b,c,d";
+var y = typeof(x);
+console.log(x);
+
+
+
+
+let number = 16;
+let type = typeof(number);
+
+
+let bigit = 9999999999999999999999;
+let y = typeof(bigit);
+
+let x = 3;
+let y = 4;
+let z = (3>4);
+let a = typeof(x);
+console.log(a);
+
+
+let num;
+console.log("type of number is:", typeof number);
+
+
+
+var x = null === undefined;
+
+console.log(tpyeof(x));
+
+
+const sym1 = Symbol();
+
+console.log(typeof(sym1));
+
+
+
+
+
+
+
+//6.
+
+var a ;
+var b ;
+var c;
+var d;
+
+//7.
+var a = "a";
+var b = 'b';
+var c = 'c';
+var d = 'd';
+
+//8.
+
+var str = ["first name : imran", 
+"last name : hashmi",
+"matrial status: single",
+"ccountry: india",
+"age: 21"];
+
+
+//9.
+
+
+var str = ["first name : imran", "last name : hashmi","matrial status: single","country: india","age: 21"];
+
+//10.
+
+var myage = 21;
+var yourage = 30;
+
+console.log("I am " + myage + "year old");
+console.log("you are" + yourage+ "year old.");
---
+++ variable.js
@@ -0,0 +1,40 @@
+// 1.This is a comment
+
+//2.Welcome to 30 days of javaScript
+
+/*3.this
+     is 
+        an 
+         multi
+            line
+                comment
+                    */
+
+4.
+var x = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+var y = typeof(x);
+console.log(y);
+
+
+var a = 3;
+var b = 10;
+var  c=Boolean (a>b);;
+console.log(c);
+
+
+
+let u;
+console.log(u);
+
+let number;
+console.log("type of number is:", typeof number);
+
+
+ var x = null === undefined;
+console.log(x);
+
+
+
+
+
+                
